22219738ff66168d5960b47a6c7c2f9e9073e02b
Daniel Curran <daniel.curran@RACKSPACE.com>
2012-02-06 15:10:37 -0600

    should be complete, hope this works

--- grails-app/resource/com/dmoney/TimeResource.groovy
+++ grails-app/resource/com/dmoney/TimeResource.groovy
@@ -5,6 +5,18 @@ import javax.ws.rs.Path
 import javax.ws.rs.Consumes
 import javax.ws.rs.Produces
 import javax.ws.rs.PathParam
+import javax.ws.rs.core.Response;
+
+
+
+import javax.ws.rs.QueryParam
+import javax.ws.rs.HeaderParam
+
+
+import static org.grails.jaxrs.response.Responses.*
+
+
+
 
 
 @Consumes(['application/xml', 'application/json'])
@@ -19,7 +31,6 @@ class TimeResource
     @Path('/time/{zone}')
     Response get( @PathParam('zone') String zone) 
     {
-        TimeConverterService.getTime(zone)
+        ok TimeConverterService.getTime(zone)
     }
-    
 }
